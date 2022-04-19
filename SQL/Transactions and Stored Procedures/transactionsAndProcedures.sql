f22525df6d8d0fed2da318c4d8c448876d1a0700
antoniotoineto <netocaastro10@gmail.com>
2022-04-18 22:54:24 -0300

    add Procedures

--- SQL/Transactions and Stored Procedures/transactionsAndProcedures.sql
+++ SQL/Transactions and Stored Procedures/transactionsAndProcedures.sql
@@ -27,3 +27,83 @@ BEGIN
     RETURN NEW;
 END;
 $inserir_quadrado_localiza_item$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_heroi() RETURNS trigger AS $inserir_heroi$
+BEGIN
+    PERFORM * FROM tbl_heroi WHERE id_heroi = new.id_heroi;
+    IF FOUND THEN
+        RAISE EXCEPTION 'Heroi já existente';
+    END IF;
+    RETURN NEW;
+END;
+$inserir_heroi$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_aliado() RETURNS trigger AS $inserir_aliado$
+BEGIN
+    PERFORM * FROM tbl_aliado WHERE id = new.id;
+    IF FOUND THEN
+        RAISE EXCEPTION 'Aliado já existente';
+    END IF;
+    RETURN NEW;
+END;
+$inserir_aliado$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_inimigo() RETURNS trigger AS $inserir_inimigo$
+BEGIN
+    PERFORM * FROM tbl_inimigo WHERE id = new.id;
+    IF FOUND THEN
+        RAISE EXCEPTION 'Inimigo já existente';
+    END IF;
+    RETURN NEW;
+END;
+$inserir_inimigo$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_civil() RETURNS trigger AS $inserir_civil$
+BEGIN
+    PERFORM * FROM tbl_civil WHERE id = new.id;
+    IF FOUND THEN
+        RAISE EXCEPTION 'Civil já existente';
+    END IF;
+    RETURN NEW;
+END;
+$inserir_civil$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_utilitario() RETURNS trigger AS $inserir_utilitario$
+BEGIN
+    PERFORM * FROM tbl_utilitario WHERE idUtilitario = new.idUtilitario;
+    IF FOUND THEN
+        RAISE EXCEPTION 'Utilitario já existente';
+    END IF;
+    RETURN NEW;
+END;
+$inserir_utilitario$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_arma() RETURNS trigger AS $inserir_arma$
+BEGIN
+    PERFORM * FROM tbl_arma WHERE idArma = new.idArma;
+    IF FOUND THEN
+        RAISE EXCEPTION 'Arma já existente';
+    END IF;
+    RETURN NEW;
+END;
+$inserir_arma$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_armadura() RETURNS trigger AS $inserir_armadura$
+BEGIN
+    PERFORM * FROM tbl_armadura WHERE idArmadura = new.idArmadura;
+    IF FOUND THEN
+        RAISE EXCEPTION 'Armadura já existente';
+    END IF;
+    RETURN NEW;
+END;
+$inserir_armadura$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION inserir_vantagens() RETURNS trigger AS $inserir_vantagens$
+BEGIN
+    PERFORM * FROM tbl_vantagens WHERE id_vantagem = new.id_vantagem;
+    IF FOUND THEN
+        RAISE EXCEPTION 'Vantagem já existente';
+    END IF;
+    RETURN NEW;
+END;
+$inserir_vantagens$ LANGUAGE plpgsql;
