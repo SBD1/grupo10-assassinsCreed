2bf808720cd0c18d83994cc1e7f09fc1fcda503b
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2022-04-24 20:12:51 -0300

    Nova versão Game

---
+++ Game/newGame/code/level.py
@@ -0,0 +1,65 @@
+import pygame 
+from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
+from debug import debug
+
+class Level:
+	def __init__(self):
+
+		# Local de movimento do boneco
+		self.display_surface = pygame.display.get_surface()
+
+		# configurações de câmera e obstáculo
+		self.visible_sprites = YSortCameraGroup()
+		self.obstacle_sprites = pygame.sprite.Group()
+
+		# abre o mapa
+		self.create_map()
+
+	def create_map(self):
+		for row_index,row in enumerate(WORLD_MAP):
+			for col_index, col in enumerate(row):
+				x = col_index * TILESIZE # Tamanho coluna e linha
+				y = row_index * TILESIZE # Tamanho coluna e linha
+				if col == 'x':
+					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
+				if col == 'p': # PLAYER LOCALIZAÇÃO SPAWN
+					self.player = Player((1500,2000),[self.visible_sprites],self.obstacle_sprites)
+
+	def run(self):
+		# Atualização coordenada enquanto move
+		self.visible_sprites.custom_draw(self.player)
+		self.visible_sprites.update()
+		debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+	def __init__(self):
+
+		# Configuração geral do tamanho
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
+
+		# Criação do mapa
+		self.floor_surf = pygame.image.load('../imagens/mapa/mapa.png').convert() # Imagem mapa
+		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
+	def custom_draw(self,player):
+
+		# Deslocamento coordenada
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
+
+		# Desenhando o chão e colisões
+		floor_offset_pos = self.floor_rect.topleft - self.offset
+		self.display_surface.blit(self.floor_surf,floor_offset_pos)
+
+		# Colisões
+		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image,offset_pos)
---
+++ Game/newGame/code/main.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from settings import *
+from level import Level
+
+#Principal
+class Game:
+	def __init__(self):
+
+		# Setup da janela windows
+		pygame.init()
+		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+		pygame.display.set_caption('Assasins Creed')
+		self.clock = pygame.time.Clock()
+
+		self.level = Level()
+
+
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+
+			self.screen.fill('black')
+			self.level.run()
+			pygame.display.update()
+			self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+	game = Game()
+	game.run()
