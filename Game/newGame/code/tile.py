b809cbde4b2565b0fe739876631fc392811441b0
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2022-04-27 21:19:08 -0300

    Atualização sincronizando mapa com os obstáculos

--- Game/newGame/code/tile.py
+++ Game/newGame/code/tile.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-	def __init__(self,pos,groups):
+	def __init__(self,pos,groups,sprite_type,surface = pygame.Surface((TILESIZE,TILESIZE))):
 		super().__init__(groups)
 		self.image = pygame.image.load('../imagens/jogador/pedra.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
